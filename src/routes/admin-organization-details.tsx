2144fd456ec97265543d5fde50e5ed200c2abd5a
PineGithub19 <114576524+PineGithub19@users.noreply.github.com>
2026-05-09 06:10:59 +0100

    chore: rename the store

--- src/routes/admin-organization-details.tsx
+++ src/routes/admin-organization-details.tsx
@@ -1,13 +1,15 @@
 import DashboardLayout from "@/layouts/dashboard-layout";
 import { useEffect } from "react";
 import { Outlet, useParams } from "react-router-dom";
-import { adminOrganizationDetailsStore } from "@/features/admin-organization-details/store/admin-organization-details";
+import { useAdminOrganizationDetailsStore } from "@/features/admin-organization-details/store/admin-organization-details";
 
 const AdminOrganizationDetails = (): React.JSX.Element => {
 	const { orgId } = useParams<{ orgId: string }>();
 
 	useEffect(() => {
-		adminOrganizationDetailsStore.getState().setOrganizationId(orgId ?? null);
+		useAdminOrganizationDetailsStore
+			.getState()
+			.setOrganizationId(orgId ?? null);
 	}, [orgId]);
 
 	return (
