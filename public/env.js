58a448b1648c40446c2320479dddfe459872a7ea
thng292venera <thong@venerian.space>
2026-06-20 06:08:27 +0700

    chore: better env defaults

--- public/env.js
+++ public/env.js
@@ -2,5 +2,5 @@ window.env = {
   gantryUrl: "http://localhost:8000",
   keycloakUrl: "http://localhost:8080",
   keycloakRealm: "gantry",
-  keycloakClientId: "gantry-frontend",
+  keycloakClientId: "gantry-admin",
 }
